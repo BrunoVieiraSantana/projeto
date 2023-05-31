d4d3abbd73f8b50d9ba886fd5879128a06785903
Bruno Vieira <125419291+BrunoVieiraSantana@users.noreply.github.com>
2023-05-31 19:56:09 -0300

    Delete Projetos directory

--- Projetos/projeto_versão_01/dashboard.py
+++
@@ -1,9 +0,0 @@
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.lang import Builder
-
-Builder.load_file('dashboard.kv')
-
-class DashboardApp(MDBoxLayout):
-    def __init__(self, **kw) -> None:
-        super().__init__(**kw)
-
